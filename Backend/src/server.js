75ca79cdc41593a91053c7552f1439a4dd1a8a1d
BrijeshDevOp <brijesh9663dev@gmail.com>
2026-09-19 22:31:18 +0530

    Prepare for deployment

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -5,18 +5,23 @@ import dotenv from "dotenv";
 import ratelimit from "./config/upstash.js";
 import rateLimiter from "./middleware/ratelimiter.js";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const _dirname = path.resolve();
+// console.log(`Path ${_dirname}`);
 
 // Required for POST/PUT requests to read JSON payloads from req.body
-app.use(
-    cors({
-        origin:"http://localhost:5173",
-    })
-);
+if(process.env.NODE_ENV !== "production"){
+    app.use(
+        cors({
+            origin:"http://localhost:5173",
+        })
+    );
+}
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // Support form-encoded request bodies too
@@ -28,6 +33,13 @@ app.use((req,res,next)=>{
 });
 
 app.use("/api/notes",notesRoutes);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(_dirname,"../frontend/dist")));
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(_dirname,"../frontend","dist","index.html"))
+    });
+}
 
 try {
     await connectDB();
